6133dab5c9253478d181091c421b0ebf1ddd9442
Atsu Vovor <atsu.vovor@gmail.com>
2026-01-05 01:25:50 -0500

    Update train_lgd.py

--- models/lgd/train_lgd.py
+++ models/lgd/train_lgd.py
@@ -1,1 +1,12 @@
+from sklearn.linear_model import LinearRegression
 
+def train_lgd(df):
+    df_d = df[df["default_flag"] == 1]
+
+    X = df_d[["fico_score", "utilization", "loan_balance"]]
+    y = df_d["lgd"]
+
+    model = LinearRegression()
+    model.fit(X, y)
+
+    return model
